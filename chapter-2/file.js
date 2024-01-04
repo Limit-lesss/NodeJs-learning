f7ccbc8b621f475349fae1f7ff4f3fa5e1d9434c
Pranav Kumar <engineersempire0204@gmail.com>
2024-01-04 11:00:39 +0530

    chapter-2

--- chapter-2/file.js
+++ chapter-2/file.js
@@ -6,5 +6,16 @@ const fs = require("fs");
 // //Async
 // fs.writeFile("./test.txt", "hello again", (err) => {});
 
-const res = fs.readFileSync("./test.txt", "utf-8");
-console.log(res);
+// const res = fs.readFileSync("./test.txt", "utf-8");
+// console.log(res);
+
+// fs.appendFileSync("./test.txt", `\n${Date.now()}`); //! add file
+
+// fs.readFile("./test.txt", "utf-8", (error, res) => {
+//   console.log(res);
+// }); //! read file
+
+// fs.cpSync("./test.txt", "./copy.txt") //! copy file
+// fs.unlinkSync("./copy.txt"); //! delete file
+
+console.log(fs.statSync("./test.txt"));
